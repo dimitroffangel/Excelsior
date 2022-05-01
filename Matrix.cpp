1e8edc2f75d56218c530f925748ba14b803d7ef4
dimitroffangel <angeldimitrov05@gmail.com>
2022-05-01 13:23:35 +0300

    added matrix class

---
+++ Matrix.cpp
@@ -0,0 +1,29 @@
+#include "Matrix.h"
+
+#include "./Core.h"
+
+#include <cassert>
+
+using namespace Excelsior;
+
+Vector3 Excelsior::operator*(const Vector3& lhs, const Matrix& rhs)
+{
+	// TODO:: Transpose the matrix first so that there are less cache misses ? 
+	// Put some tests to record if this is necessary 
+
+	const int vectorSize = 3;
+	assert(vectorSize == rhs.MATRIX_SIZE);
+
+	const std::vector<Real> vectorCoordinates = { lhs.m_X, lhs.m_Y, lhs.m_Z };
+	std::vector<Real> resultedVectorCoordinates = { 0, 0, 0 };
+	for (size_t col = 0; col < vectorSize; ++col)
+	{
+		for (size_t i = 0; i < vectorSize; ++i)
+		{
+			resultedVectorCoordinates[col] += vectorCoordinates[i] * rhs.m_Matrix[col][i];
+		}
+	}
+
+	Vector3 result = Vector3(resultedVectorCoordinates[0], resultedVectorCoordinates[1], resultedVectorCoordinates[2]);
+	return result;
+}
---
+++ Matrix.h
@@ -0,0 +1,60 @@
+#ifndef EXCELSIOR_MATRIX_H
+#define EXCELSIOR_MATRIX_H
+
+#include "./Core.h"
+#include <vector>
+#include <cassert>
+
+namespace Excelsior
+{
+	class Matrix
+	{
+		static const size_t MATRIX_SIZE = 3;
+
+	private:
+		Matrix()
+		{
+			m_Matrix.resize(MATRIX_SIZE);
+			for (size_t i = 0; i < MATRIX_SIZE; ++i)
+			{
+				m_Matrix[i].resize(MATRIX_SIZE);
+			}
+		}
+
+	public:
+		Matrix(const std::vector<std::vector<Real>> matrix)
+			: m_Matrix(matrix.begin(), matrix.end())
+		{
+			assert(matrix.size() == MATRIX_SIZE);
+			assert(matrix[0].size() == MATRIX_SIZE);
+		}
+
+		Matrix operator* (const Matrix& rhs)
+		{
+			// TODO:: Transpose the matrix first so that there are less cache misses ? 
+			// Put some tests to record if this is necessary 
+			
+			Matrix result;
+			
+			for (size_t row = 0; row < MATRIX_SIZE; ++row)
+			{
+				for (size_t col = 0; col < MATRIX_SIZE; ++col)
+				{
+					for (size_t multiplyingIndex = 0; multiplyingIndex < MATRIX_SIZE; ++multiplyingIndex)
+					{
+						result.m_Matrix[row][col] += m_Matrix[row][multiplyingIndex] * m_Matrix[multiplyingIndex][col];
+					}
+				}
+			}
+
+			return result;
+		}
+
+		friend Vector3 operator*(const Vector3& lhs, const Matrix& rhs);
+
+	private:
+		std::vector<std::vector<Real>> m_Matrix;
+	};
+}
+
+#endif
